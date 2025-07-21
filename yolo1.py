291f1adb8aefc7bb1a7148cb40452516694bcb4a
Guruvaraprasad1225 <guruvaraprasad.borra@gmail.com>
2025-07-21 12:09:24 -0400

    Refactored local project structure and updated file locations

--- yolo1.py
+++
@@ -1,8 +0,0 @@
-import cv2
-VideoAccess = cv2.VideoCapture(0)
-ret, frame = VideoAccess.read()
-cv2.imshow("Webcam",frame)
-if cv2.waitkey(1)==ord('q'):
-    break
-VideoAccess.release()
-cv2.destroyAllWindows()
